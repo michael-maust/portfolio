5eeaa78b282319930798f7c3a3939bd8275c4cba
Michael Maust <65863230+Designerman717@users.noreply.github.com>
2021-11-10 15:44:18 -0600

    Pre-Tailwind Push

--- website/src/App.js
+++ website/src/App.js
@@ -1,24 +1,74 @@
-
+import Styled from 'styled-components'
 import './App.css';
+import Test from './Components/test'
+
+
+const Wrapper = Styled.div `
+
+.App {
+  text-align: center;
+}
+
+.App-logo {
+  height: 40vmin;
+  pointer-events: none;
+}
+
+@media (prefers-reduced-motion: no-preference) {
+  .App-logo {
+    animation: App-logo-spin infinite 20s linear;
+  }
+}
+
+
+p, a {
+
+  padding: 1rem;
+
+}
+
+.App-header {
+  background-color: #282c;
+  min-height: 10vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+
+}
+
+.App-link {
+  color: #61dafb;
+}
+
+@keyframes App-logo-spin {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+
+
+
+`
+
 
 function App() {
   return (
+    <Wrapper>
     <div className="App">
       <header className="App-header">
         
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+      <Test />
+
       </header>
     </div>
+    </Wrapper>
   );
 }
 
--- website/src/Components/test.js
+++ website/src/Components/test.js
@@ -1,19 +1,52 @@
-import React from 'react'
+import React from 'react';
+import Styled from 'styled-components';
+
+const Wrapper = Styled.div`
 
 
-
-
-
-const test = () => {
-    return (
-        <div>
-            
-
-
-
-
-        </div>
-    )
+p {
+    font-size: 20px;
+    color: red;
 }
 
-export default test
+h1 {
+    color: black;
+}
+
+
+
+.card {
+
+    background-color: white;
+    margin: 100px;
+    padding: 100px;
+    border-radius: 50px;
+    inline-size: 300px;
+    overflow-wrap: break-word;
+    box-shadow: 50px 10px 80px 5px black;
+
+
+
+}
+
+`;
+
+const test = () => {
+  return (
+    <Wrapper>
+      <div>
+        <div className="card">
+          <h1>HEADER</h1>
+          <p>
+            This is a test paragraph. I am a person. Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit. Atque expedita harum quis cumque amet
+            earum eius dignissimos corrupti distinctio hic dolores, nisi enim
+            odio quam totam cupiditate, autem maxime nulla?
+          </p>
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
+
+export default test;
